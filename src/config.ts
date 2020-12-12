d820b3cbee8cf2fc718715dc107e49f54d327627
Matthew Volz <matthew.c.volz@gmail.com>
2020-12-12 11:57:51 -0800

    move to new domain

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
     oAuthClientId: '340317273424-6voils0o5kt47t86ugo1rv124rgcbsuv.apps.googleusercontent.com',
     oAuthClientSecret: process.env.oAuthClientSecret,
-    oAuthCallbackUrl: 'https://geo-frame.herokuapp.com/auth/google/callback',
+    oAuthCallbackUrl: 'https://geo-frame.com/auth/google/callback',
     scopes: [
         'https://www.googleapis.com/auth/photoslibrary.readonly',
         'profile',

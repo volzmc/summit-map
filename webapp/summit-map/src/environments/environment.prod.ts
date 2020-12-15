f70cb3a842133ba054a391c7a24c3c1fffbd9f9c
VOLZ-DESKTOP\matth <matthew.c.volz@gmail.com>
2020-12-14 19:35:31 -0800

    forward path

--- webapp/summit-map/src/environments/environment.prod.ts
+++ webapp/summit-map/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   maps_api_key: 'AIzaSyAEljaAT_5TW2vZKqA9s9_XyQ_fmiZgEKo',
-  apiBase: 'https://geo-frame.herokuapp.com/api',
+  apiBase: 'https://geo-frame.com/api',
   base: 'https://geo-frame.com'
 };
